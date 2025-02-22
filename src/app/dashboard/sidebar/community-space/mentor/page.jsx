c5909df66d4ac398a1895cc8190ded0de762cae0
Shrikant <jadhashrikant@gmail.com>
2025-02-23 03:45:04 +0530

    hi

--- src/app/dashboard/sidebar/community-space/mentor/page.jsx
+++ src/app/dashboard/sidebar/community-space/mentor/page.jsx
@@ -1,21 +1,11 @@
-"use client"
-import { Experience } from "@/components/Disha/Experience";
-import { UI } from "@/components/Disha/UI";
-import { ChatProvider } from "@/hooks/useChat";
-import { Loader } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
-import { Leva } from "leva";
-import './style.css'
-function App() {
-  return (<>
-      <Loader />
-      <Leva   hidden/>
-      <UI />
-      <Canvas className="" shadows camera={{ position: [0, 0, 1], fov: 30 }}>
-        <Experience />
-      </Canvas>
-  </>
-  );
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      hi
+    </div>
+  )
 }
 
-export default App;
+export default page
